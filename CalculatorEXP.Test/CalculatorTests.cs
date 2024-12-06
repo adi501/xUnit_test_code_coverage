3cd5f53fa71030e5fa01120716727cbd32744ebf
Adinarayana Reddy JC <jc.adi101@gmail.com>
2024-12-06 21:59:12 +0530

    Add project files.

---
+++ CalculatorEXP.Test/CalculatorTests.cs
@@ -0,0 +1,23 @@
+namespace CalculatorEXP.Test
+{
+    public class CalculatorTests
+    {
+        private readonly Calculator _calculator = new Calculator();
+        [Fact]
+        public void GivenTwoNumbers_Add_ReturnsTheTwoNumbersAddedTogether()
+        {
+            var one = 1;
+            var two = 2;
+            var actual = _calculator.Add(one, two);
+            Assert.Equal(3, actual);
+        }
+        [Fact]
+        public void GivenTwoNumbers_Subtract_ReturnsTheFirstNumberSubtractedFromTheSecond()
+        {
+            var one = 1;
+            var two = 2;
+            var actual = _calculator.Subtract(one, two);
+            Assert.Equal(-1, actual);
+        }
+    }
+}
---
+++ CalculatorEXP/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorEXP
+{
+    public class Calculator
+    {
+        public int Add(int one, int two)
+        {
+            return one + two;
+        }
+        public int Subtract(int one, int two)
+        {
+            return one - two;
+        }
+    }
+}
